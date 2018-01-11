5948defd1dd7a1382b034c9dc38e1278a892a97b
JeanEspindola <jean.espindola@gmail.com>
2018-01-10 22:04:11 -0200

    Adding store and reducer.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import configureStore from './store/configureStore';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+//import { loadCartList } from './actions/cartListAction';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+//store.dispatch(loadCartList());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
