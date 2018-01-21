932d70df61f1fba081a18eb0e4b8ddbf1377f1b3
JeanEspindola <jean.espindola@gmail.com>
2018-01-21 20:30:57 -0200

    Updating dependencies. Updating snapshots.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,9 +2,10 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import jsdom from 'jsdom';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.document = doc;
-global.window = doc.defaultView;
+const { JSDOM } = jsdom;
 
+const { document } = (new JSDOM('')).window;
+global.document = document;
+global.window = document.defaultView;
 
 configure({ adapter: new Adapter() });
