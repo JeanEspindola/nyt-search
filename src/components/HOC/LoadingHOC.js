de51c618ec33f7f931aad3e3676cd98fdfe057c8
JeanEspindola <jean.espindola@gmail.com>
2018-01-21 20:40:08 -0200

    Creating HOC for handle list empty and in-progress.

---
+++ src/components/HOC/LoadingHOC.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './LoadingHOC.css';
+
+const isEmpty = prop => (
+  prop === null || prop === undefined || prop.length === 0
+);
+
+const LoaderHOC = (listProp, loadingIndicator) =>
+  (WrappedComponent) => class LoadingHOC extends Component {
+    render() {
+      if (this.props[loadingIndicator]) return <div className="loader"></div>;
+
+      return (isEmpty(this.props[listProp])) ? <p>There are no results to display.</p> :
+      <WrappedComponent {...this.props} />;
+    }
+  };
+
+
+export default LoaderHOC;
