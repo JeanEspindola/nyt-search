794d97991cd627729e203e4f6ae59e525c03a7e9
JeanEspindola <jean.espindola@gmail.com>
2018-02-25 00:50:45 -0300

    Adding snapshots to action test.

--- src/actions/articleSearchAction.test.js
+++ src/actions/articleSearchAction.test.js
@@ -1,18 +1,52 @@
 import { SEARCH_SUBMIT_SUCCESS, LOADING_INDICATOR_SUCCESS } from '../utils/constants';
-import { searchQuerySuccess, loadingSubmit } from './articleSearchAction';
+import { searchQuerySuccess, loadingSubmit, loadSearchResults } from './articleSearchAction';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
 
 describe('articleSearchAction', () => {
-  it('Action --> SEARCH_SUBMIT_SUCCESS', () => {
-    const list = [{ article: 'a' }, { article: 'b' }, { article: 'c' }];
-    const page = 1;
-    const query = 'q';
-    expect(searchQuerySuccess(list, page, query)).toEqual({
-      type: SEARCH_SUBMIT_SUCCESS, list, page, query,
-    });
+  let store;
+  beforeEach(() => {
+    store = mockStore({});
   });
 
-  it('Action --> LOADING_INDICATOR_SUCCESS', () => {
-    const loading = true;
-    expect(loadingSubmit(loading)).toEqual({ type: LOADING_INDICATOR_SUCCESS, loading });
+  it('creates a LOADING_INDICATOR_SUCCESS action', () => {
+    expect(searchQuerySuccess([], 1, 'test')).toEqual(
+      {
+        type: SEARCH_SUBMIT_SUCCESS,
+        list: [],
+        page: 1,
+        query: 'test',
+      }
+    );
+    expect(searchQuerySuccess([], 1, true)).toMatchSnapshot();
+  });
+
+  it('creates a SEARCH_SUBMIT_SUCCESS action', () => {
+    expect(loadingSubmit(false)).toEqual(
+      {
+        type: LOADING_INDICATOR_SUCCESS,
+        loading: false,
+      }
+    );
+    expect(loadingSubmit(false)).toMatchSnapshot();
+  });
+
+  it('should handle loadSearchResults action - success', async () => {
+    const response = [
+      {
+        article: 'a', snippet: 'a',
+      },
+      {
+        article: 'b', snippet: 'b',
+      },
+      {
+        article: 'c', snippet: 'c',
+      },
+    ];
+    fetch.mockResponseSuccess(response);
+    await store.dispatch(loadSearchResults('test', 1));
+    expect(store.getActions()).toMatchSnapshot();
   });
 });
