fadcbf8c81dc693d68325a50c553dd1054eb39e9
JeanEspindola <jean.espindola@gmail.com>
2018-03-03 23:43:03 -0300

    Fixing lint and removing unused file.

--- src/components/HOC/LoadingHOC.js
+++ src/components/HOC/LoadingHOC.js
@@ -6,14 +6,13 @@ const isEmpty = prop => (
 );
 
 const LoaderHOC = (listProp, loadingIndicator) =>
-  (WrappedComponent) => class LoadingHOC extends Component {
+  WrappedComponent => class LoadingHOC extends Component {
     render() {
-      if (this.props[loadingIndicator]) return <div className="loader"></div>;
+      if (this.props[loadingIndicator]) return <div className="loader" />;
 
       return (isEmpty(this.props[listProp])) ? <p>There are no results to display.</p> :
       <WrappedComponent {...this.props} />;
     }
   };
-
 
 export default LoaderHOC;
