53f243631c03be7a3ce25147418a837bd7208cdb
JeanEspindola <jean.espindola@gmail.com>
2018-03-06 22:54:06 -0300

    Updating test to use snapshot.

--- src/reducers/articleSearchReducer.test.js
+++ src/reducers/articleSearchReducer.test.js
@@ -3,57 +3,42 @@ import initialState from './initialState';
 import { LOADING_INDICATOR_SUCCESS, SEARCH_SUBMIT_SUCCESS } from '../utils/constants';
 
 describe('cartListReducer', () => {
-  let state;
-  let defaultState;
-
-  beforeEach(() => {
-    state = initialState;
-    defaultState = {
-      page: 0,
-      query: '',
-      loading: false,
-      list: [],
-    };
-  });
+  let action;
 
   it('Reducer --> SEARCH_SUBMIT_SUCCESS', () => {
-    const list = [
-      {
-        article: 'a', snippet: 'a',
-      },
-      {
-        article: 'b', snippet: 'b',
-      },
-      {
-        article: 'c', snippet: 'c',
-      },
-    ];
-    const page = 3;
-    const query = 'test';
-    state = articleList(state, {
-      type: SEARCH_SUBMIT_SUCCESS, list, page, query,
-    });
-    expect(state.list).toEqual(list);
-    expect(state.page).toEqual(page);
-    expect(state.query).toEqual(query);
+    action = {
+      list: [
+        {
+          article: 'a', snippet: 'a',
+        },
+        {
+          article: 'b', snippet: 'b',
+        },
+        {
+          article: 'c', snippet: 'c',
+        },
+      ],
+      page: 3,
+      query: 'test',
+      type: SEARCH_SUBMIT_SUCCESS,
+    };
+    expect(articleList(initialState, action)).toMatchSnapshot();
   });
 
   it('Reducer --> LOADING_INDICATOR_SUCCESS', () => {
-    const loading = true;
-    state = articleList(state, {
-      type: LOADING_INDICATOR_SUCCESS, loading,
-    });
-    expect(state.loading).toEqual(loading);
+    action = {
+      type: LOADING_INDICATOR_SUCCESS,
+      loading: true,
+    };
+
+    expect(articleList(initialState, action)).toMatchSnapshot();
   });
 
   it('Reducer --> DEFAULT', () => {
-    state = initialState;
-    state = articleList(state, { type: '' });
-    expect(state).toEqual(defaultState);
+    expect(articleList(initialState, { type: '' })).toMatchSnapshot();
   });
 
   it('Reducer --> return initial state', () => {
-    state = articleList(undefined, {});
-    expect(state).toEqual(initialState);
+    expect(articleList(undefined, {})).toMatchSnapshot();
   });
 });
