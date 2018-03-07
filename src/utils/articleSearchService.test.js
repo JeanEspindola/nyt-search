ed7a3efd71e7d7d48bbfcdb93655fc60d88b839d
JeanEspindola <jean.espindola@gmail.com>
2018-03-06 23:08:18 -0300

    Using reusable mock functions and adding coverage on service error.

--- src/utils/articleSearchService.test.js
+++ src/utils/articleSearchService.test.js
@@ -4,27 +4,22 @@ import articleMockedData from '../mock/articleMockedData.json';
 describe('articleSearchService', () => {
   const list = articleMockedData.responseList;
 
-  const responseObj = {
-    response: {
-      docs: articleMockedData.responseList,
-    },
-  };
+  it('call getSearchResults - success', async () => {
+    const ret = articleMockedData.responseList;
 
-  const jsonFunc = () => responseObj;
+    fetch.mockResponseSuccess(ret);
 
-  beforeEach(() => {
-    global.fetch = jest.fn().mockImplementation(() => new Promise((resolve) => {
-      resolve({
-        ok: true,
-        Id: '123',
-        json: jsonFunc,
-      });
-    }));
+    const response = await articleSearchService.getSearchResults('search', 1);
+    expect(response).toBe(list.response.docs);
   });
 
-  it('mock api call mocking function', async () => {
+  it('call getSearchResults - error', async () => {
+    const error = new Error('error on service call');
+
+    fetch.mockResponseFailure(error);
+
     const response = await articleSearchService.getSearchResults('search', 1);
-    expect(response).toBe(list);
+    expect(response).toBe(error);
   });
 
   it('add id to each element of the list', () => {
