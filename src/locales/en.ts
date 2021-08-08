c62960039b08edfe05c06688f65dc2eef8e80d22
JeanEspindola <jean.espindola@gmail.com>
2021-08-08 15:22:18 +0200

    Deleting snapshots. Adding React-Intl.

---
+++ src/locales/en.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  goToResults: "Go to Results Page",
+  headerTitle: "\"The New York Times\" article search application",
+  nextPage: "Next Page",
+  noDetails: "No details to show.",
+  noResults: "There are no results to display.",
+  previousPage: "Previous Page",
+  readArticle: "Read the Full Article",
+  results: "Results:",
+  searchTerms: "Search your terms here.",
+  typeQuery: "Type search query term in here:"
+}
---
+++ src/locales/index.ts
@@ -0,0 +1,9 @@
+import enTranslationMessages from './en'
+import ptbrTranslationMessages from './pt-br'
+
+export const translationMessages : {
+	[languageKey: string] : { [translationKey: string]: string};
+} = {
+	en: enTranslationMessages,
+	pt_BR: ptbrTranslationMessages,
+}
